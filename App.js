bca88a9fdc90988f51b76178db289f63f9207663
Anurag Bhandari <anuragbhandari77736@gmail.com>
2024-01-13 15:01:19 +0530

    Progress saved

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { useState } from 'react';
+import { useState,useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View,Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import EmojiList from './components/EmojiList';
 import Button from './components/Button';
@@ -12,12 +12,21 @@ import CircleButton from './components/CircleButton';
 import IconButton from './components/IconButton';
 import EmojiPicker from './components/EmojiPicker';
 import EmojiSticker from './components/EmojiSticker';
+import * as MediaLibrary from 'expo-media-library'
+import { captureRef } from 'react-native-view-shot';
+import domtoimage from 'dom-to-image'
 export default function App() {
+  <StatusBar style="light" />
+
+  const [status,requestPermission] = MediaLibrary.usePermissions()
+  if(status ===null){
+    requestPermission()
+  }
   const [selectedImage, setSelectedImage] = useState(null);
   const [showAppOptions,setShowAppOptions] = useState(false)
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [pickedEmoji, setPickedEmoji] = useState(null);
-  
+  const imageRef = useRef()
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
@@ -42,8 +51,42 @@ console.log(result);
   };
 
   const onSaveImageAsync = async () => {
-    // we will implement this later
-  };
+    if(Platform.OS !=='web') {
+       try {
+      const localUri = await captureRef(imageRef,{
+        height:440,
+        quality:1
+      })
+      await MediaLibrary.saveToLibraryAsync(localUri)
+      if(localUri){
+        alert("Saved")
+      }
+      
+    } catch (error) {
+      console.log(e);
+    }
+  }
+else{
+  try {
+    const dataUrl = await domtoimage.toJpeg(imageRef.current,{
+      quality:1,
+      width:320,
+      height:440
+    })
+    let link = document.createElement('a')
+    link.download = 'sticker-smash.jpeg'
+    link.href=dataUrl
+    link.click()
+    
+  } catch (error) {
+    console.log(error);
+    
+  }
+}
+
+    }
+    
+   
   const onModalClose = ()=>
   {
   setIsModalVisible(false)
@@ -55,8 +98,12 @@ console.log(result);
       <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
       <View style={styles.imageContainer}>
+         <View ref={imageRef} collapsable={false}>
+
+        
         <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
         {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji}/>}
+      </View>
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -2,6 +2,8 @@ import { View,Image } from "react-native";
 import { Gesture,GestureDetector } from "react-native-gesture-handler";
 import Animated,{useAnimatedStyle,useSharedValue,withSpring} from "react-native-reanimated";
 export default function EmojiSticker({imageSize,stickerSource}) {
+  const translateX = useSharedValue(0)
+  const translateY = useSharedValue(0)
   const scaleImage = useSharedValue(imageSize)
   const imageStyle = useAnimatedStyle(()=>{
     return{
@@ -17,8 +19,26 @@ export default function EmojiSticker({imageSize,stickerSource}) {
     scaleImage.value = scaleImage.value*2
   }
 })
+const drag = Gesture.Pan().onChange((event)=>
+{
+  translateX.value+=event.changeX
+  translateY.value+=event.changeY
+})
+const containerStyle = useAnimatedStyle(()=>{
+  return{
+    transform:[
+      {
+        translateX:translateX.value
+      },
+      {
+        translateY:translateY.value                                       
+      }
+    ]
+  }                                          
+})
     return(
-    <View style={{ top: -350 }}>
+      <GestureDetector gesture={drag}>
+    <Animated.View style={[containerStyle,{ top: -350 }]}>
       <GestureDetector gesture={doubleTap}>
     <Animated.Image
       source={stickerSource}
@@ -26,6 +46,7 @@ export default function EmojiSticker({imageSize,stickerSource}) {
       style={[imageStyle,{ width: imageSize, height: imageSize }]}
     />
     </GestureDetector>
-  </View>)
+  </Animated.View>
+  </GestureDetector>)
     
 }
